403d0a2cd6d95a61bee094f74dfef0c6be4da704
liambilly9 <99061606+liambilly9@users.noreply.github.com>
2022-07-24 08:19:45 +0300

    used weblient instead of httpclient

--- navigation sample/navigation sample/Page1.xaml.cs
+++ navigation sample/navigation sample/Page1.xaml.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,59 +17,65 @@ namespace navigation_sample
         public Page1()
         {
             InitializeComponent();
+
+            //using (WebClient client = new WebClient())
+            //{
+            //    string ulr = client.DownloadString("htt ps://www.azlyrics.com/lyrics/chrisbrown/next2you.html");
+            //    lyrics.Text=ulr;
+            //}
+
+            //HtmlWebViewSource htmlWebViewSource = new HtmlWebViewSource();
+            //htmlWebViewSource.BaseUrl="http s://www.azlyrics.com/lyrics/chrisbrown/next2you.html";
+            //lsource.Source="http s://www.azlyrics.com/lyrics/chrisbrown/next2you.html";
         }
 
         private void show_Clicked(object sender, EventArgs e)
         {
-            //try
-            //{
-            //    using (var client = new HttpClient())
-            //    {
-            //        var ulr = new Uri("https://www.azlyrics.com/lyrics/chrisbrown/next2you.html");
-            //        StringBuilder lyricsonly = new StringBuilder();
-            //        var url = client.GetAsync(ulr).Result;
-            //        //var s=ulr.AbsoluteUri.Replace(ulr.Query,"");
-            //        var json = url.Content.ReadAsStringAsync().Result;
-            //        #region stringbuilding
-            //        var str = Regex.Replace(json, @"<script[^>]*>[\s\S]*?</script>", string.Empty);
-            //        var newstr = Regex.Replace(str, "<.*?>", string.Empty);
-            //        ////var trimmed = newstr.Trim();
-            //        //int edited = newstr.IndexOf("Search");
-            //        //int from = newstr.IndexOf("Submit Corrections");
-            //        //int to = newstr.Length-1;
-            //        //var subbed = newstr.Substring(edited, from);
-            //        //lyricsonly.AppendLine(subbed);
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string ulr = client.DownloadString("https://www.azlyrics.com/lyrics/chrisbrown/next2you.html");
+                    //lyrics.Text=ulr;
+                    StringBuilder lyricsonly = new StringBuilder();
+                    #region stringbuilding
+                    var str = Regex.Replace(ulr, @"<script[^>]*>[\s\S]*?</script>", string.Empty);
+                    var newstr = Regex.Replace(str, "<.*?>", string.Empty);
+                    //var trimmed = newstr.Trim();
+                    int edited = newstr.IndexOf("Search");
+                    int from = newstr.IndexOf("Submit Corrections");
+                    //int to = newstr.Length-1;
+                    var subbed = newstr.Substring(edited, from);
+                    lyricsonly.AppendLine(subbed);
 
 
-            //        //lyricsonly.Replace(",", Environment.NewLine);
-            //        ////lyricsonly.Remove(from, to);
-            //        ////lyricsonly.Remove(0, 6);
-            //        //lyricsonly.Replace("AZLyrics", "");
-            //        //string v = Regex.Replace(lyricsonly.ToString(), @"\s+", "").Trim();
-            //        //string u = lyricsonly.ToString();
-            //        //string w = u.Trim();
-            //        //StringBuilder x = new StringBuilder();
-            //        //foreach (char c in lyricsonly.ToString())
-            //        //{
-            //        //    if (!char.IsWhiteSpace(c))
-            //        //    {
-            //        //        x.Append(c);
-            //        //    }
-            //        //}
-            //        //string y = x.ToString();
-            //        #endregion
-            //        //var tostring = JsonConvert.SerializeObject(json);
-                    
-            //        lyrics.Text=newstr;
-            //    }
+                    lyricsonly.Replace(",", Environment.NewLine);
+                    //lyricsonly.Remove(from, to);
+                    //lyricsonly.Remove(0, 6);
+                    lyricsonly.Replace("AZLyrics", "");
+                    string v = Regex.Replace(lyricsonly.ToString(), @"\s+", "").Trim();
+                    string u = lyricsonly.ToString();
+                    string w = u.Trim();
+                    StringBuilder x = new StringBuilder();
+                    foreach (char c in lyricsonly.ToString())
+                    {
+                        if (!char.IsWhiteSpace(c))
+                        {
+                            x.Append(c);
+                        }
+                    }
+                    string y = x.ToString();
+                    #endregion
 
-            //}
-            //catch (Exception ex)
-            //{
-            //    lyrics.Text=ex.Message;
+                    lyrics.Text=newstr;
+                }
 
-            //}
-            Shell.Current.GoToAsync(nameof(Page2login));
+            }
+            catch (Exception ex)
+            {
+                lyrics.Text=ex.Message;
+
+            }
         }
     }
 }
